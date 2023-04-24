71866de8a5274828c6c9bca863523d0fd8299291
tnk6 <98718391+tnk6@users.noreply.github.com>
2023-04-24 16:52:07 -0400

    Add files via upload

---
+++ home.php
@@ -0,0 +1,37 @@
+<html>
+<body>
+<h1>Select Location</h1>
+<form onsubmit="return true" method="POST">
+    <input type="radio" id="1" name="location" value="New York">
+    <label for="1">New York</label><br>
+    <input type="radio" id="2" name="location" value="Philadelphia">
+    <label for="2">Philadelphia</label><br>
+    <input type="radio" id="3" name="location" value="Boston">
+    <label for="3">Boston</label><br>
+    <input type="radio" id="4" name="location" value="Ho Chi Minh">
+    <label for="4">Ho Chi Minh</label><br>
+    <input type="radio" id="5" name="location" value="Baltimore">
+    <label for="5">Baltimore</label><br>
+    <input type="submit" class="mt-3 btn btn-primary" value="Show Cars" />
+</form>
+<ul>
+    <?php
+        if (isset($_POST["location"])) {
+            $cnx = new mysqli('localhost', 'root', 'Chicken1!', 'rentacar'); 
+
+            if ($cnx->connect_error)
+                die('Connection failed: ' . $cnx->connect_error);
+        
+            $query = 'SELECT * FROM CAR WHERE BID=' . $_POST["email"];
+            $cursor = $cnx->query($query);
+            while ($row = $cursor->fetch_assoc()) {
+                echo '<li>';
+                echo $row['Year'] . ' ' . $row['MName'];
+                //echo '<button onclick="window.location.href = \'compare.php?id=' . $row['id'] . '\';">Buy</button>';
+                echo '</li>';
+            }
+        }
+    ?>
+</ul>
+</body>
+</html>
